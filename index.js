05a9084cf56fa26924f73fd3c1d86a0089d7402a
Avara, Emre <emre_avara@hotmail.com>
2022-07-31 21:50:31 +0200

    Remove unnecessary switch case

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
 
 let result = "";
 let temp = { tempResult : "", tempOperator:""};
+
 function onClickNumber(number){
     result = `${result}${number}`;
     setResult(result);
 }
 
-function setResult(result){
+function setResult(result){    
     document.getElementById("result").innerHTML = result;
 }
 
 function onClickOperation(operator){
-    switch(operator){
-        case "+":
-            temp.tempResult = result;
-            temp.tempOperator = "+";
-            result = ""
-            break;
-        case "-":
-            temp.tempResult = result;
-            temp.tempOperator = "-";
-            result = ""
-            break;
-        case "/":
-            temp.tempResult = result;
-            temp.tempOperator = "/";
-            result = ""
-            break;
-        case "*":
-            temp.tempResult = result;
-            temp.tempOperator = "*";
-            result = ""
-            break;
-        case "=":
-            result = calculateResult(temp.tempOperator, Number(temp.tempResult), Number(result))
+    if(operator === "="){
+        result = calculateResult(temp.tempOperator, Number(temp.tempResult), Number(result))
+        setResult(result);
+    } else{
+        if(temp.tempResult !== ""){
+            result = calculateResult(temp.tempOperator, Number(temp.tempResult), Number(result));
             setResult(result);
-            break;
+        } 
+        temp.tempResult = result;
+        temp.tempOperator = operator;
+        result = ""
+        
     }
+    console.log(`temp operation : ${temp.tempOperator} temp result : ${temp.tempResult} result : ${result}`)
 }
 
 function calculateResult(operator, num1, num2){
+    temp.tempOperator = "";
+    temp.tempResult   = "";
     switch(operator){
         case "+":
             return num1 + num2;
@@ -54,7 +45,7 @@ function calculateResult(operator, num1, num2){
             return num1 * num2;
             break;
         default:
-            return 0;
+            return result;
             break;
     }
 }
